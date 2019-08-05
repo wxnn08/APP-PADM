b5974de1de2fe298122a7c25a2f9378e0232577a
Wesley <wesleypersilva@gmail.com>
2019-08-05 18:04:36 -0300

    add topbar catalogo

--- Fucas/app/src/main/java/com/wesley/fucas/Catalogo.kt
+++ Fucas/app/src/main/java/com/wesley/fucas/Catalogo.kt
@@ -1,7 +1,10 @@
 package com.wesley.fucas
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,6 +16,7 @@ class Catalogo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalogo_lojas)
+        setSupportActionBar(findViewById(R.id.simple_appbar))
 
 
         bindComponents()
